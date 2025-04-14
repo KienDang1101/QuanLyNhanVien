28e3c1686d7769ca2ca718d5f1e3242192a8d86a
KienDang1101 <kientrung40021101@gmail.com>
2025-04-14 23:22:45 +0700

    Update GhiFileCSV.cpp

--- GhiFileCSV.cpp
+++ GhiFileCSV.cpp
@@ -1,16 +1,14 @@
 #include "GhiFileCSV.h"
 
-vector<NhanVien*> danhSachNhanVien;
-
 void GhiFileCSV::GhiFile(){
     ofstream myFile("DanhSach_out.csv");
 
     if (myFile.is_open()) {
         myFile << "ID,Ten,Chuc vu,Ngay bat dau lam,Ngay lam viec,Ngay nghi phep tich luy,Ngay nghi khong phep,Luong co ban,He so luong,Luong thuc te,Danh gia\n";
 
-        for (auto nv : danhSachNhanVien) {
-            myFile << nv->getID() << "," << nv->getHoTen() << "," << nv->getChuCVu() << "," << nv->getNgayLam() << "," << nv->getNgayLamViec() << ","
-                    << nv->getNgayPhepTichLuy() << "," << nv->getNgaynghiKhongPhep() << "," << nv->getLuongCoBan() << ","
+        for (auto nv : DanhSachNhanVien) {
+            myFile << nv->getID() << "," << nv->getHoTen() << "," << nv->getChucVu() << "," << nv->getNgayLam() << "," << nv->getNgayLamViec() << ","
+                    << nv->getNgayPhepTichLuy() << "," << nv->getNgayNghiKhongPhep() << "," << nv->getLuongCoBan() << ","
                     << nv->HeSoLuong() << "," << nv->TinhLuong() << "," << nv->DanhGia() << "\n";
             }
 
@@ -18,6 +16,6 @@ void GhiFileCSV::GhiFile(){
         cout << "\nViet file thanh cong !\n";
     }
     else {
-        out << "Khong tao duoc file\n";
+        cout << "Khong tao duoc file\n";
     }
 }
