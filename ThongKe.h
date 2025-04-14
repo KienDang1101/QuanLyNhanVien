0f18f160766aa6caf34ec3fb5010838be75b8d68
kien172 <91733054+kien172@users.noreply.github.com>
2025-04-14 23:15:20 +0700

    Update ThongKe.h

--- ThongKe.h
+++ ThongKe.h
@@ -7,6 +7,8 @@ class ThongKe {
 public:
 	vector <NhanVien*> List_DanhGia(vector <NhanVien*>, string);
 
+	vector <NhanVien*> List_NgayNghi(vector <NhanVien*>, unsigned int);
+
 	vector <NhanVien*> Options(vector <NhanVien*>);
 };
 
