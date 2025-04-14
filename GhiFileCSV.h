d0e69f9fb60db0055167ad31226c9699f8171ac9
topstrong89 <92913823+topstrong89@users.noreply.github.com>
2025-04-14 22:53:30 +0700

    Add files via upload

---
+++ GhiFileCSV.h
@@ -0,0 +1,17 @@
+#ifndef GHIFILECSV_H
+#define GHIFILECSV_H
+
+#include "NhanVien.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class GhiFileCSV(){
+    void GhiFile();
+};
+
+#endif
