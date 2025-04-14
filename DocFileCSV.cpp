5d48ae806f8b6096299570b9fbfcd5e8de83f5cc
topstrong89 <92913823+topstrong89@users.noreply.github.com>
2025-04-14 20:41:55 +0700

    Add files via upload

---
+++ DocFileCSV.cpp
@@ -0,0 +1,61 @@
+#include "DocFileCSV.h"
+
+void DocFileCSV::DocFile(){
+    for (auto nv : danhSachNhanVien) delete nv;
+        danhSachNhanVien.clear();
+
+        ifstream myFile("DanhSach.csv");
+
+        if (myFile.is_open()) {
+            string line;
+            getline(myFile, line);
+
+            while (getline(myFile, line)) {
+                stringstream ss(line);
+                string tempString;
+                string chucvu,hoten,ngaylam;
+                NhanVien* nv = nullptr;
+
+                getline(ss, tempString, ','); int id = stoi(tempString);
+                getline(ss, hoten, ',');
+                getline(ss, chucvu, ',');
+                getline(ss, ngaylam, ',');
+                getline(ss, tempString, ','); int ngaylamviec = stoi(tempString);
+                getline(ss, tempString, ','); int ngayphep = stoi(tempString);
+                getline(ss, tempString, ','); int nghikp = stoi(tempString);
+                getline(ss, tempString, ','); int luongcb = stoi(tempString);
+
+                if (chucvu == "Nhan vien") nv = new NhanVienVanPhong();
+                else if (chucvu == "Pho phong") nv = new PhoPhong();
+                else if (chucvu == "Truong phong") nv = new TruongPhong();
+
+                if (nv) {
+                    nv->setID(id);
+                    nv->setHoTen(hoten);
+                    nv->setChucVu(chucvu);
+                    nv->setNgayLam(ngaylam);
+                    nv->setNgayLamViec(ngaylamviec);
+                    nv->setNgayPhepTichLuy(ngayphep);
+                    nv->setNgayNghikhongPhep(nghikp);
+
+                    if (NhanVienVanPhong* vp = dynamic_cast<NhanVienVanPhong*>(nv))
+                        vp->setLuongCoBan(luongcb);
+                    else if (PhoPhong* pp = dynamic_cast<PhoPhong*>(nv))
+                        pp->setLuongCoBan(luongcb);
+                    else if (TruongPhong* tp = dynamic_cast<TruongPhong*>(nv))
+                        tp->setLuongCoBan(luongcb);
+
+                    // nv->setHeSoLuong(nv->HeSoLuong());
+                    // nv->setLuongThucTe(nv->TinhLuong());
+                    // nv->setDanhGia(nv->DanhGia());
+
+                    danhSachNhanVien.push_back(nv);
+                }
+            }
+            cout << "\nDoc file thanh cong\n";
+            myFile.close();
+        }
+        else {
+            cout << "\nKhong mo duoc file\n";
+        }
+}
---
+++ DocFileCSV.h
@@ -0,0 +1,12 @@
+#ifndef DOCFILECSV_H
+#define DOCFILECSV_H
+
+#include "NhanVien.h"
+#include "NhanVien.cpp"
+
+using namespace std;
+
+class DocFileCSV(){
+    public:
+    static void DocFile();
+}
