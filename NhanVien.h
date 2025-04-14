c9950e71ca3c286a5e5d6f63b86e9214d027935e
KienDang1101 <kientrung40021101@gmail.com>
2025-04-14 20:32:13 +0700

    Update NhanVien.h

--- NhanVien.h
+++ NhanVien.h
@@ -1,206 +1,91 @@
-#include "NhanVien.h"
+#ifndef NHANVIEN_H
+#define NHANVIEN_H
 
-NhanVien::NhanVien()
-{
-	this->ID = 0;
-	this->HoTen = "";
-	this->NgayLam = "";
-	this->ChucVu = "";
-	this->NgayLamViec = 0;
-	this->NgayPhepTichLuy = 0;
-	this->NgayNghiKhongPhep = 0;
-}
+#include <iostream>
+#include <string>
 
-NhanVien::NhanVien(int ID, string HoTen, string NgayLam, string ChucVu, int NgayLamViec, int NgayPhepTichLuy, int NgayNghiKhongPhep)
-{
-	setID(ID);
-	setHoTen(HoTen);
-	setChucVu(ChucVu);
-	setNgayLam(NgayLam);
-	setNgayLamViec(NgayLamViec);
-	setNgayPhepTichLuy(NgayPhepTichLuy);
-	setNgayNghiKhongPhep(NgayNghiKhongPhep);
-}
+using namespace std;
 
-NhanVien::~NhanVien() {}
+class NhanVien
+{
+protected:
+	int ID;
+	string HoTen;
+	string NgayLam;
+	string ChucVu;
+	int NgayLamViec;
+	int NgayPhepTichLuy;
+	int NgayNghiKhongPhep;
+public:
+	NhanVien();
+	NhanVien(int, string, string, string, int, int, int);
+	~NhanVien();
 
-void NhanVien::setID(int ID)
-{
-	this->ID = ID;
-}
-int NhanVien::getID() const
-{
-	return this->ID;
-}
+	void setID(int);
+	int getID() const;
 
-void NhanVien::setHoTen(string HoTen)
-{
-	this->HoTen = HoTen;
-}
-string NhanVien::getHoTen() const
-{
-	return this->HoTen;
-}
+	void setHoTen(string);
+	string getHoTen() const;
 
-void NhanVien::setNgayLam(string NgayLam)
-{
-	this->NgayLam = NgayLam;
-}
-string NhanVien::getNgayLam() const
-{
-	return this->NgayLam;
-}
+	void setNgayLam(string);
+	string getNgayLam() const;
 
-void NhanVien::setNgayLamViec(int NgayLamViec)
-{
-	this->NgayLamViec = NgayLamViec;
-}
-int NhanVien::getNgayLamViec() const
-{
-	return this->NgayLamViec;
-}
+	void setChucVu(string);
+	string getChucVu() const;
 
-void NhanVien::setNgayPhepTichLuy(int NgayPhepTichLuy)
-{
-	this->NgayPhepTichLuy = NgayPhepTichLuy;
-}
-int NhanVien::getNgayPhepTichLuy() const
-{
-	return this->NgayPhepTichLuy;
-}
+	void setNgayLamViec(int);
+	int getNgayLamViec() const;
 
-void NhanVien::setNgayNghiKhongPhep(int NgayNghiKhongPhep)
-{
-	this->NgayNghiKhongPhep = NgayNghiKhongPhep;
-}
-int NhanVien::getNgayNghiKhongPhep() const
-{
-	return this->NgayNghiKhongPhep;
-}
+	void setNgayPhepTichLuy(int);
+	int getNgayPhepTichLuy() const;
 
-string NhanVien::DanhGia()
-{
-	if (this->NgayLamViec >= 20)
-		return "Tot";
-	else if (this->NgayLamViec >= 15)
-		return "TrungBinh";
-	else
-		return "Yeu";
-}
+	void setNgayNghiKhongPhep(int);
+	int getNgayNghiKhongPhep() const;
 
-float NhanVien::HeSoLuong()
-{
-	float result = 0.0f;
-	if (this->NgayLamViec / 260 > 10)
-		result = 2.0f;
-	else if (this->NgayLamViec / 260 > 5)
-		result = 1.5f;
-	else
-		result = 1.0f;
-	return result;
-}
+	string DanhGia();
+	float HeSoLuong();
+	virtual float TinhLuong();
+};
 
-TruongPhong::TruongPhong()
+class TruongPhong : public NhanVien
 {
-	this->LuongCoBan = 0;
-}
+private:
+	int LuongCoBan;
+public:
+	TruongPhong();
+	TruongPhong(int, string, string, string, int, int, int, int);
+	~TruongPhong();
 
-TruongPhong::TruongPhong(int ID, string HoTen, string NgayLam, string ChucVu, int NgayLamViec, int NgayPhepTichLuy, int NgayNghiKhongPhep, int LuongCoBan)
-	: NhanVien(ID, HoTen, NgayLam, ChucVu, NgayLamViec, NgayPhepTichLuy, NgayNghiKhongPhep)
+	void setLuongCoBan(int);
+	int getLuongCoBan() const;
+	float TinhLuong();
+};
+
+class PhoPhong : public NhanVien
 {
-	this->LuongCoBan = LuongCoBan;
-}
+private:
+	int LuongCoBan;
+public:
+	PhoPhong();
+	PhoPhong(int, string, string, string, int, int, int, int);
+	~PhoPhong();
 
-TruongPhong::~TruongPhong() {}
+	void setLuongCoBan(int);
+	int getLuongCoBan() const;
+	float TinhLuong();
+};
 
-void TruongPhong::setLuongCoBan(int LuongCoBan)
+class NhanVienVanPhong : public NhanVien
 {
-	this->LuongCoBan = LuongCoBan;
-}
-int TruongPhong::getLuongCoBan() const
-{
-	return this->LuongCoBan;
-}
+private:
+	int LuongCoBan;
+public:
+	NhanVienVanPhong();
+	NhanVienVanPhong(int, string, string, string, int, int, int, int);
+	~NhanVienVanPhong();
 
-PhoPhong::PhoPhong()
-{
-	this->LuongCoBan = 0;
-}
-
-PhoPhong::PhoPhong(int ID, string HoTen, string NgayLam, string ChucVu, int NgayLamViec, int NgayPhepTichLuy, int NgayNghiKhongPhep, int LuongCoBan)
-	: NhanVien(ID, HoTen, NgayLam, ChucVu, NgayLamViec, NgayPhepTichLuy, NgayNghiKhongPhep)
-{
-	this->LuongCoBan = LuongCoBan;
-}
-
-PhoPhong::~PhoPhong() {}
-
-void PhoPhong::setLuongCoBan(int LuongCoBan)
-{
-	this->LuongCoBan = LuongCoBan;
-}
-int PhoPhong::getLuongCoBan() const
-{
-	return this->LuongCoBan;
-}
-
-NhanVienVanPhong::NhanVienVanPhong()
-{
-	this->LuongCoBan = 0;
-}
-
-NhanVienVanPhong::NhanVienVanPhong(int ID, string HoTen, string NgayLam, string ChucVu, int NgayLamViec, int NgayPhepTichLuy, int NgayNghiKhongPhep, int LuongCoBan)
-	: NhanVien(ID, HoTen, NgayLam, ChucVu, NgayLamViec, NgayPhepTichLuy, NgayNghiKhongPhep)
-{
-	this->LuongCoBan = LuongCoBan;
-}
-
-NhanVienVanPhong::~NhanVienVanPhong() {}
-
-void NhanVienVanPhong::setLuongCoBan(int LuongCoBan)
-{
-	this->LuongCoBan = LuongCoBan;
-}
-int NhanVienVanPhong::getLuongCoBan() const
-{
-	return this->LuongCoBan;
-}
-
-float TruongPhong::TinhLuong()
-{
-	float Luong = 0.0f;
-	Luong = LuongCoBan * HeSoLuong();
-
-	if (this->NgayNghiKhongPhep > 5)
-		Luong -= 0.1f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 3)
-		Luong -= 0.05f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 1)
-		Luong -= 0.02f * LuongCoBan;
-}
-
-float PhoPhong::TinhLuong()
-{
-	float Luong = 0.0f;
-	Luong = LuongCoBan * HeSoLuong();
-	if (this->NgayNghiKhongPhep > 5)
-		Luong -= 0.1f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 3)
-		Luong -= 0.05f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 1)
-		Luong -= 0.02f * LuongCoBan;
-	return Luong;
-}
-
-float NhanVienVanPhong::TinhLuong()
-{
-	float Luong = 0.0f;
-	Luong = LuongCoBan * HeSoLuong();
-	if (this->NgayNghiKhongPhep > 5)
-		Luong -= 0.1f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 3)
-		Luong -= 0.05f * LuongCoBan;
-	else if (this->NgayNghiKhongPhep > 1)
-		Luong -= 0.02f * LuongCoBan;
-	return Luong;
-}
+	void setLuongCoBan(int);
+	int getLuongCoBan() const;
+	float TinhLuong();
+};
+#endif
