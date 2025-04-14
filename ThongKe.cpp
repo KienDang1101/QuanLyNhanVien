5b792894cd4fd4b1cdebe1ff3728cc7c809854b6
kien172 <91733054+kien172@users.noreply.github.com>
2025-04-14 23:15:52 +0700

    Update ThongKe.cpp

--- ThongKe.cpp
+++ ThongKe.cpp
@@ -1,4 +1,4 @@
-﻿#include"ThongKe.h"
+#include"ThongKe.h"
 
 vector <NhanVien*> ThongKe::List_DanhGia(vector<NhanVien*> DanhSach, string BacDanhGia) {
 	vector <NhanVien*> ThongKeNV;
@@ -9,25 +9,51 @@ vector <NhanVien*> ThongKe::List_DanhGia(vector<NhanVien*> DanhSach, string BacD
 	return ThongKeNV;
 }
 
+vector <NhanVien*> List_NgayNghi(vector <NhanVien*> DanhSach, int Max) {
+	vector <NhanVien*> ThongKeNV;
+	for (NhanVien* NV : DanhSach) {
+		if (NV->getNgayPhepTichLuy() <= Max)
+			ThongKeNV.push_back(NV);
+	}
+	return ThongKeNV;
+}
+
 vector <NhanVien*> ThongKe::Options(vector <NhanVien*> DanhSach) {
-	unsigned int BacDanhGia;
+	unsigned int c, BacDanhGia;
+	int Max;
 	vector <NhanVien*> List;
-	cout << "-----Tim va xuat danh sach nhan vien theo danh gia-----\n";
-	cout << "1. Danh gia tot.\n";
-	cout << "2. Danh gia trung binh.\n";
-	cout << "3. Danh gia yeu.\n";
+
+	cout << "1. Tim va xuat danh sach nhan vien theo danh gia.";
+	cout << "2. Tim va xuat danh sach nhan vien co so ngay nghi khong phep.";
 	cout << "nhap lua chon: ";
-	cin >> BacDanhGia;
-	switch (BacDanhGia)
+	cin >> c;
+	switch (c)
 	{
 	case 1:
-		List = List_DanhGia(DanhSach, "Tot");
+		cout << "-----Tim va xuat danh sach nhan vien theo danh gia-----\n";
+		cout << "1. Danh gia tot.\n";
+		cout << "2. Danh gia trung binh.\n";
+		cout << "3. Danh gia yeu.\n";
+		cout << "nhap lua chon: ";
+		cin >> BacDanhGia;
+		switch (BacDanhGia)
+		{
+		case 1:
+			List = List_DanhGia(DanhSach, "Tot");
+			break;
+		case 2:
+			List = List_DanhGia(DanhSach, "TrungBinh");
+			break;
+		case 3:
+			List = List_DanhGia(DanhSach, "Yeu");
+			break;
+		}
 		break;
 	case 2:
-		List = List_DanhGia(DanhSach, "TrungBinh");
-		break;
-	case 3:
-		List = List_DanhGia(DanhSach, "Yeu");
+		cout << "-----Tim va xuat danh sach nhan vien theo so ngay nghi khong phep-----\n";
+		cout << "Nhap so ngay nghi khong phep toi da: ";
+		cin >> Max;
+		List = List_NgayNghi(DanhSach, Max);
 		break;
 	}
 	return List;
