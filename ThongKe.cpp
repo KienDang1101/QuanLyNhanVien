06742d21ef49476cdf84f0cf173dff7c71a727ea
kien172 <91733054+kien172@users.noreply.github.com>
2025-04-14 20:44:35 +0700

    Add files via upload

---
+++ ThongKe.cpp
@@ -0,0 +1,34 @@
+﻿#include"ThongKe.h"
+
+vector <NhanVien*> ThongKe::List_DanhGia(vector<NhanVien*> DanhSach, string BacDanhGia) {
+	vector <NhanVien*> ThongKeNV;
+	for (NhanVien* NV : DanhSach) {
+		if (NV->DanhGia() == BacDanhGia)
+			ThongKeNV.push_back(NV);
+	}
+	return ThongKeNV;
+}
+
+vector <NhanVien*> ThongKe::Options(vector <NhanVien*> DanhSach) {
+	unsigned int BacDanhGia;
+	vector <NhanVien*> List;
+	cout << "-----Tim va xuat danh sach nhan vien theo danh gia-----\n";
+	cout << "1. Danh gia tot.\n";
+	cout << "2. Danh gia trung binh.\n";
+	cout << "3. Danh gia yeu.\n";
+	cout << "nhap lua chon: ";
+	cin >> BacDanhGia;
+	switch (BacDanhGia)
+	{
+	case 1:
+		List = List_DanhGia(DanhSach, "Tot");
+		break;
+	case 2:
+		List = List_DanhGia(DanhSach, "TrungBinh");
+		break;
+	case 3:
+		List = List_DanhGia(DanhSach, "Yeu");
+		break;
+	}
+	return List;
+}
---
+++ ThongKe.h
@@ -0,0 +1,13 @@
+#ifndef THONGKE_H
+#define THONGKE_H
+
+#include "NhanVien.h"
+
+class ThongKe {
+public:
+	vector <NhanVien*> List_DanhGia(vector <NhanVien*>, string);
+
+	vector <NhanVien*> Options(vector <NhanVien*>);
+};
+
+#endif
